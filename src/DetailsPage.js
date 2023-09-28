39719ccdf216d103e4de67df4d179c5a532a9310
Reid Mccrabb <reidmccrabb@gmail.com>
2023-09-28 10:33:10 -0500

    details page layout

--- src/DetailsPage.js
+++ src/DetailsPage.js
@@ -11,12 +11,24 @@ function DetailsPage({ skills }) {
 
     // Render skill details
     return (
-        <div>
-            <h2>{skill.name}</h2>
-            <p>Avg. Salary: {skill.avgSalary}</p>
-            <p>1 Day % Change: {skill.oneDayChange}</p>
-            <p>7 Day % Change: {skill.sevenDayChange}</p>
-            {/* ... add more details here */}
+        <div className="details-wrapper">
+            <div className="skill-details">
+                <h2>{skill.name}</h2>
+                <p>Avg. Salary: {skill.avgSalary}</p>
+                <p>1 Day % Change: {skill.oneDayChange}</p>
+                <p>7 Day % Change: {skill.sevenDayChange}</p>
+                {/* ... add more details here */}
+            </div>
+
+            <div className="news-section">
+                <h3>News on {skill.name}</h3>
+                {/* Add your news content here */}
+            </div>
+
+            <div className="courses-section">
+                <h3>Courses on {skill.name}</h3>
+                {/* Add your courses content here */}
+            </div>
         </div>
     );
 }
