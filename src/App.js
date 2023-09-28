2d9acc3cb7716e2728c1a5b03d4c6769f2f492e5
Reid Mccrabb <reidmccrabb@gmail.com>
2023-09-27 23:15:29 -0500

    random inconsistent variable ticking

--- src/App.js
+++ src/App.js
@@ -111,128 +111,153 @@ function App() {
   const [skills, setSkills] = useState(initialSkills);
 
   useEffect(() => {
-    const tickSkills = () => {
-      setSkills((prevSkills) =>
-        prevSkills.map((skill) => ({
-          ...skill,
-          avgSalary: `$${(
-            parseFloat(skill.avgSalary.replace("$", "").replace(",", "")) +
-            Math.random() * 500 -
-            250
-          )
-            .toFixed(2)
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`,
-          oneDayChange: `${(
-            parseFloat(skill.oneDayChange) +
-            Math.random() * 0.4 -
-            0.2
-          ).toFixed(2)}%`,
-          sevenDayChange: `${(
-            parseFloat(skill.sevenDayChange) +
-            Math.random() * 0.4 -
-            0.2
-          ).toFixed(2)}%`,
-          oneYearChange: `${(
-            parseFloat(skill.oneYearChange) +
-            Math.random() * 0.4 -
-            0.2
-          ).toFixed(2)}%`,
-          marketValue: `$${(
-            parseFloat(skill.marketValue.replace("$", "").replace("B", "")) +
-            Math.random() * 0.05 -
-            0.025
-          ).toFixed(1)}B`,
-          openings: `${(
-            parseFloat(skill.openings.replace(",", "")) +
-            Math.random() * 50 -
-            25
-          )
-            .toFixed(0)
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`,
-          workers: `${(
-            parseFloat(skill.workers.replace(",", "")) +
-            Math.random() * 1000 -
-            500
-          )
-            .toFixed(0)
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`,
-        }))
-      );
+    const updateSingleSkill = (index) => {
+      setSkills((prevSkills) => {
+        const skill = prevSkills[index];
+        return [
+          ...prevSkills.slice(0, index),
+          {
+            ...skill,
+            avgSalary: `$${(
+              parseFloat(skill.avgSalary.replace("$", "").replace(",", "")) +
+              Math.random() * 500 -
+              250
+            )
+              .toFixed(2)
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`,
+            oneDayChange: `${(
+              parseFloat(skill.oneDayChange) +
+              Math.random() * 0.4 -
+              0.2
+            ).toFixed(2)}%`,
+            sevenDayChange: `${(
+              parseFloat(skill.sevenDayChange) +
+              Math.random() * 0.4 -
+              0.2
+            ).toFixed(2)}%`,
+            oneYearChange: `${(
+              parseFloat(skill.oneYearChange) +
+              Math.random() * 0.4 -
+              0.2
+            ).toFixed(2)}%`,
+            marketValue: `$${(
+              parseFloat(skill.marketValue.replace("$", "").replace("B", "")) +
+              Math.random() * 0.05 -
+              0.025
+            ).toFixed(1)}B`,
+            openings: `${(
+              parseFloat(skill.openings.replace(",", "")) +
+              Math.random() * 50 -
+              25
+            )
+              .toFixed(0)
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`,
+            workers: `${(
+              parseFloat(skill.workers.replace(",", "")) +
+              Math.random() * 1000 -
+              500
+            )
+              .toFixed(0)
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`,
+          },
+          ...prevSkills.slice(index + 1),
+        ];
+      });
     };
 
-    const intervalId = setInterval(tickSkills, 5000); // Update every 5 seconds
+    const tickSkillRandomly = () => {
+      const randomIndex = Math.floor(Math.random() * skills.length);
+      updateSingleSkill(randomIndex);
 
-    return () => clearInterval(intervalId); // Cleanup on unmount
-  }, []);
+      // Set next random update after current update finishes
+      setupRandomTick();
+    };
+
+    const setupRandomTick = () => {
+      // Random duration between 1 to 5 seconds
+      const randomDuration = Math.floor(Math.random() * 4000) + 1000;
+      setTimeout(tickSkillRandomly, randomDuration);
+    };
+
+    // Start the first tick
+    setupRandomTick();
+
+    // If you want to cleanup the timeout when the component unmounts:
+    return () => {
+      clearTimeout(tickSkillRandomly);
+    };
+  }, []); // Note: Empty dependency array
 
   return (
     <Router>
-        <div className="App">
-            <header className="App-header">
-                <h1>Skill Rank</h1>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <>
-                                <div className="skills-list">
-                                    <div className="header-row">
-              <span>Rank (#)</span>
-              <span>Skill Name</span>
-              <span>Avg. Salary</span>
-              <span>1 Day % Change</span>
-              <span>7 Day % Change</span>
-              <span>1 Year % Change</span>
-              <span>Total Market Value</span>
-              <span>Total Openings</span>
-              <span>Total # of Workers</span>
-            </div>
-            {skills.map((skill, index) => (
-                                            <Link
-                                                className="skill-link"
-                                                to={`/details/${skill.name.replace(/\s+/g, "-").toLowerCase()}`}
-                                                key={index}
-                                            >
-                                                <div className="skill-item">
-                  <span className="rank">{index + 1}</span>
-                  <img
-                    src={`${process.env.PUBLIC_URL}/${
-                      skill.name === "Web Developer"
-                        ? "webdev.png"
-                        : skill.name === "Comedy Actor"
-                        ? "comedy.png"
-                        : skill.name === "Brain Surgeon"
-                        ? "brain.png"
-                        : "ai.png"
-                    }`}
-                    alt="Skill Icon"
-                    className="skill-icon"
-                  />
+      <div className="App">
+        <header className="App-header">
+          <h1>Skill Rank</h1>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <div className="skills-list">
+                    <div className="header-row">
+                      <span>Rank (#)</span>
+                      <span>Skill Name</span>
+                      <span>Avg. Salary</span>
+                      <span>1 Day % Change</span>
+                      <span>7 Day % Change</span>
+                      <span>1 Year % Change</span>
+                      <span>Total Market Value</span>
+                      <span>Total Openings</span>
+                      <span>Total # of Workers</span>
+                    </div>
+                    {skills.map((skill, index) => (
+                      <Link
+                        className="skill-link"
+                        to={`/details/${skill.name
+                          .replace(/\s+/g, "-")
+                          .toLowerCase()}`}
+                        key={index}
+                      >
+                        <div className="skill-item">
+                          <span className="rank">{index + 1}</span>
+                          <img
+                            src={`${process.env.PUBLIC_URL}/${
+                              skill.name === "Web Developer"
+                                ? "webdev.png"
+                                : skill.name === "Comedy Actor"
+                                ? "comedy.png"
+                                : skill.name === "Brain Surgeon"
+                                ? "brain.png"
+                                : "ai.png"
+                            }`}
+                            alt="Skill Icon"
+                            className="skill-icon"
+                          />
 
-                  <span className="skill-name">{skill.name}</span>
-                  <span>{skill.avgSalary}</span>
-                  <span>{skill.oneDayChange}</span>
-                  <span>{skill.sevenDayChange}</span>
-                  <span>{skill.oneYearChange}</span>
-                  <span>{skill.marketValue}</span>
-                  <span>{skill.openings}</span>
-                  <span>{skill.workers}</span>
+                          <span className="skill-name">{skill.name}</span>
+                          <span>{skill.avgSalary}</span>
+                          <span>{skill.oneDayChange}</span>
+                          <span>{skill.sevenDayChange}</span>
+                          <span>{skill.oneYearChange}</span>
+                          <span>{skill.marketValue}</span>
+                          <span>{skill.openings}</span>
+                          <span>{skill.workers}</span>
+                        </div>
+                      </Link>
+                    ))}
                   </div>
-                                            </Link>
-                                        ))}
-                                    </div>
-                                </>
-                            }
-                        />
-                        <Route
-                            path="/details/:skillName"
-                            element={<DetailsPage skills={skills} />}
-                        />
-                    </Routes>
-                </header>
-            </div>
-        </Router>
-    );
+                </>
+              }
+            />
+            <Route
+              path="/details/:skillName"
+              element={<DetailsPage skills={skills} />}
+            />
+          </Routes>
+        </header>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
