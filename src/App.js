912fef4fe5c98fb207f31c89c9c56a21f5c43270
Reid Mccrabb <reidmccrabb@gmail.com>
2023-09-27 20:32:43 -0500

    fluctuating numbers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const skills = [
+  const initialSkills = [
     {
       name: "Software Development",
       avgSalary: "$120,000",
@@ -104,6 +104,29 @@ function App() {
       workers: "490,000"
     },
   ];
+
+  const [skills, setSkills] = useState(initialSkills);
+
+  useEffect(() => {
+    const tickSkills = () => {
+      setSkills(prevSkills => 
+        prevSkills.map(skill => ({
+          ...skill,
+          avgSalary: `$${(parseFloat(skill.avgSalary.replace('$', '').replace(',', '')) + Math.random() * 1000 - 500).toFixed(2)}`,
+          oneDayChange: `${(parseFloat(skill.oneDayChange) + Math.random() * 2 - 1).toFixed(2)}%`,
+          sevenDayChange: `${(parseFloat(skill.sevenDayChange) + Math.random() * 2 - 1).toFixed(2)}%`,
+          oneYearChange: `${(parseFloat(skill.oneYearChange) + Math.random() * 2 - 1).toFixed(2)}%`,
+          marketValue: `$${(parseFloat(skill.marketValue.replace('$', '').replace('B', '')) + Math.random() * 0.1 - 0.05).toFixed(1)}B`,
+          openings: `${(parseFloat(skill.openings.replace(',', '')) + Math.random() * 100 - 50).toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`,
+          workers: `${(parseFloat(skill.workers.replace(',', '')) + Math.random() * 1000 - 500).toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`
+        }))
+      );
+    };
+
+    const intervalId = setInterval(tickSkills, 3000);  // Update every 3 seconds
+
+    return () => clearInterval(intervalId);  // Cleanup on unmount
+  }, []);
 
   return (
     <div className="App">
