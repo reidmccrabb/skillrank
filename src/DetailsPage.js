9b44c220b69152c9546a8357a29467d186cb0fc3
Reid Mccrabb <reidmccrabb@gmail.com>
2023-09-28 17:21:44 -0500

    live news

--- src/DetailsPage.js
+++ src/DetailsPage.js
@@ -1,9 +1,27 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function DetailsPage({ skills }) {
     const { skillName } = useParams();
 
+    const [newsArticles, setNewsArticles] = useState([]);
     const skill = skills.find(s => s.name.replace(/\s+/g, "-").toLowerCase() === skillName);
+
+    useEffect(() => {
+        const API_KEY = 'gj0XOFRwhSKdmP_qPP_VlYlF4hWOMHvba2IKrQiyFP3B2jgy'; // Replace with your actual API key
+        const url = `https://api.currentsapi.services/v1/latest-news?language=en&category=technology&keywords=${skill.name}&apiKey=${API_KEY}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data && data.news && data.news.length > 0) {
+                    setNewsArticles(data.news.slice(0, 3)); // Get the first three articles
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching news:", error);
+            });
+    }, [skill.name]);
 
     if (!skill) {
         return <div>Skill not found!</div>;
@@ -27,13 +45,15 @@ function DetailsPage({ skills }) {
 
             <div className="news-section">
                 <h3>News on {skill.name}</h3>
-                <article className="news-article">
-                    <img src="/sample-news-image.jpg" alt="Sample News" className="news-image" />
-                    <h4>The Rise of the {skill.name} role in 2023</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at odio nec ligula facilisis lacinia.</p>
-                    <a href="/news-article-1">Read more</a>
-                </article>
-                {/* ... you can add more articles in a similar format */}
+                {newsArticles.map(article => (
+                    <article key={article.id} className="news-article">
+                        <img src={article.image} alt={article.title} className="news-image" />
+                        <h4>{article.title}</h4>
+                        <p>{article.description}</p>
+                        <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
+                    </article>
+                ))}
+
             </div>
 
             <div className="courses-section">
